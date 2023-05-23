6b2d3816624fcbe7d30cbcec52b905b9f1483fa1
Nils <87240201+Sliner05@users.noreply.github.com>
2023-05-23 08:29:00 +0200

    erstes dagramm versucht

--- app/backend/chart_compare/chart.model.js
+++ app/backend/chart_compare/chart.model.js
@@ -1,5 +1,17 @@
-let data = axios.get("https://data.tg.ch/api/v2/catalog/datasets/sk-stat-10/exports/json").then(
+let apidata = axios.get("https://data.tg.ch/api/v2/catalog/datasets/sk-stat-10/exports/json").then(makeChart)
 
-
-
-)
+function makeChart() {
+    let label1 = 'joe'
+    let label2 = 'who'
+    let chartobj = document.getElementById(lineChart)
+    let lineChart = new Chart(chartobj, {
+        type: 'line',
+        data: {
+            labels: {},
+            datasets: [{
+                label: label1,
+                data: [1, 2, 3, 5, 6, 7, 4, 7]
+            }]
+        }
+    });
+}
--- app/frontend/scripts/main.js
+++ app/frontend/scripts/main.js
@@ -2,3 +2,23 @@ SVGInject.setOptions({makeIdsUnique: false});
 const elMapTg = document.getElementById('tg-map');
 SVGInject(elMapTg);
 
+let apidata = axios.get("https://data.tg.ch/api/v2/catalog/datasets/sk-stat-10/exports/json").then(makeChart)
+
+function makeChart() {
+    let label1 = 'joe'
+    let label2 = 'who'
+    let chartobj = document.getElementById(lineChart)
+    let lineChart = new Chart(chartobj, {
+        type: 'line',
+        data: {
+            labels: [1, 2, 3, 4, 5],
+            datasets: [{
+                label: label1,
+                data: [1, 2, 3, 5, 6]
+            },{
+                label: label2,
+                data: [5, 3, 7, 5, 9]
+            }]
+        }
+    });
+}
