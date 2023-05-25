0622351ca39c968d908a98a7568021ce2e35cfb1
Nils <87240201+Sliner05@users.noreply.github.com>
2023-05-25 09:57:22 +0200

    added last json function (compare)

--- app/backend/chart_compare/chart.model.js
+++ app/backend/chart_compare/chart.model.js
@@ -1,21 +1,45 @@
 const axios = require('axios')
 
-axios.get("https://data.tg.ch/api/v2/catalog/datasets/sk-stat-10/exports/json").then(res => processCompareData(res.data))
+axios.get("https://data.tg.ch/api/v2/catalog/datasets/sk-stat-10/exports/json").then(res => processCompareData(res))
 
+function processCompareData(data, newParam = []) {
 
-function processCompareData(data){
+    let parties = ['svp', 'fdp', 'cvp', 'sp', 'gp', 'glp', 'evp', 'edu', 'bdp', 'uebrige']
 
     let processedData = []
+    let total = 0
+    data = data.data
+    let votesAllYears = []
+    for (let year of ['2008', '2012', '2016', '2020']){
+        let partiesYear = [];
+        for (let party of parties){
+            let datayear = data.filter(obj=>obj.wahljahr===year);
+            for (entry of datayear){
+                total += entry[party]
+            }
+            partiesYear.push({name: party, votes: total})
+            total = 0
+        }
+        votesAllYears.push({year: year, parties: partiesYear});
+    }
+    console.log(JSON.stringify(votesAllYears))
 
-
-
-    return processedData;
+    console.log(processedData)
+    return processedData
 }
 
-// DATA_FORMAT
+//DATA_FORMAT:
 /*
-year:
-parties: {[
-{party: , votes:}
-]}
- */
+{{
+    [
+        {
+            year:
+            parties:
+    [
+        {name: , votes:}
+
+    ]
+},
+]
+}
+*/
